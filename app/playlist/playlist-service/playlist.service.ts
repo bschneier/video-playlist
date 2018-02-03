59ce6719ff8d7dc3238569d9e9a73c66d6e2033e
Brian Schneier <bschneier6@gmail.com>
2018-02-03 10:25:33 -0600

    Upgrade to Angular5, add video files, update video list

--- app/playlist/playlist-service/playlist.service.ts
+++ app/playlist/playlist-service/playlist.service.ts
@@ -9,22 +9,21 @@ const videoPath = require.context('./../../../assets/videos', false);
 export class PlaylistService implements IPlaylistService {
 
   getPlaylist(): Video[] {
-    const muriloVideo = './murilo.mp4';
     return this.shufflePlaylist([
-      new Video(4, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=uuYNqalkDdI') ],
-        imagePath('./chase-budinger.png'), 'Chase Budinger Bounce'),
-      new Video(19, [ new VideoSource('video/mp4', videoPath(muriloVideo)) ],
-        imagePath('./murilo.png'), 'Murilo Bounce'),
-      new Video(42, [ new VideoSource('video/mp4', 'https://www.dropbox.com/s/caulkc78v8bfnxo/grbic.mp4?dl=1') ],
-        imagePath('./grbic.png'), 'Vladimir Grbic great save and house'),
-      new Video(49, [ new VideoSource('video/mp4', videoPath('./tarasenko.mp4')) ],
+      new Video(4, [ new VideoSource('video/mp4', videoPath('./chase-budinger.mp4')) ],
+        imagePath('./chase-budinger.png'), 'Chase Budinger Spike'),
+      new Video(19, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=mtpx0PjrjoY') ],
+        imagePath('./murilo.png'), 'Murilo Spike'),
+      new Video(42, [ new VideoSource('video/mp4', videoPath('./grbic.mp4')) ],
+        imagePath('./grbic.png'), 'Vladimir Grbic great save and block'),
+      new Video(49, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=iyRGH7HDs7s') ],
         imagePath('./tarasenko.png'), 'Vladimir Tarasenko goal'),
-      new Video(41, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=Amf7Qu4pC8c') ],
-        imagePath('./giba.png'), 'Giba Digs'),
-      new Video(28, [ new VideoSource('video/mp4', 'https://www.dropbox.com/s/ygqeoi9pimrd3xr/dante.mp4?dl=1') ],
-        imagePath('./dante.png'), 'Dante Bounce'),
-      new Video(241, [ new VideoSource('video/mp4', 'https://www.dropbox.com/s/64fgvj887i9wa5f/spikes.mp4?dl=1') ],
-        imagePath('./spikes.png'), 'Face shots')
+      new Video(44, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=DojCj2xxwjA') ],
+        imagePath('./giba.png'), 'Giba Spike'),
+      new Video(28, [ new VideoSource('video/mp4', videoPath('./dante.mp4')) ],
+        imagePath('./dante.png'), 'Dante Spike'),
+      new Video(25, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=uosTUGDIdx0') ],
+        imagePath('./julio-jones.jpg'), 'Julio Jones Pregame Catch')
     ]);
   }
 
