45b7cabb88421f1912ba282d7004ddc8c5e0c502
Brian Schneier <bschneier6@gmail.com>
2018-02-17 15:05:37 -0600

    Adjust main header styling

--- app/core/header/header.component.ts
+++ app/core/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import './header.component.scss';
 
 @Component({
   selector: 'video-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public isIphoneStandalone = false;
+
   constructor() { }
+
+  ngOnInit() {
+    if ((navigator as any).standalone) {
+      this.isIphoneStandalone = true;
+    }
+  }
 }
