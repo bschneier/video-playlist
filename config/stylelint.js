802319b7710deebf77d100a325e30950cc3d69d9
Brian Schneier <bschneier6@gmail.com>
2018-02-18 17:57:34 -0600

    Update stylelint rules and fix warnings

--- config/stylelint.js
+++ config/stylelint.js
@@ -6,6 +6,14 @@ module.exports = {
     'stylelint-config-sass-guidelines'
   ],
   rules: {
-    'no-missing-end-of-source-newline': null
+    'no-missing-end-of-source-newline': null,
+    'order/properties-alphabetical-order': null,
+    'number-leading-zero': null,
+    'at-rule-no-vendor-prefix': null,
+    'property-no-vendor-prefix': null,
+    'color-named': null,
+    'selector-list-comma-newline-after': null,
+    'at-rule-no-unknown': null,
+    'max-nesting-depth': null
   }
 }
