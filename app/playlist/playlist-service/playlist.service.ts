c54afd3fe14addd031727e0ab2731985e2f754b1
Brian Schneier <bschneier6@gmail.com>
2018-02-22 15:29:59 -0600

    Update videos

--- app/playlist/playlist-service/playlist.service.ts
+++ app/playlist/playlist-service/playlist.service.ts
@@ -10,20 +10,22 @@ export class PlaylistService implements IPlaylistService {
 
   getPlaylist(): Video[] {
     return this.shufflePlaylist([
-      new Video(4, [ new VideoSource('video/mp4', videoPath('./chase-budinger.mp4')) ],
-        imagePath('./chase-budinger.png'), 'Chase Budinger Spike'),
-      new Video(19, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=mtpx0PjrjoY') ],
-        imagePath('./murilo.png'), 'Murilo Spike'),
-      new Video(42, [ new VideoSource('video/mp4', videoPath('./grbic.mp4')) ],
-        imagePath('./grbic.png'), 'Vladimir Grbic great save and block'),
+      new Video(62, [ new VideoSource('video/mp4', videoPath('./edmonds.mp4')) ],
+        imagePath('./edmonds.jpg'), 'Jim Edmonds Catch'),
+      new Video(73, [ new VideoSource('video/mp4', videoPath('./faulk.mp4')) ],
+        imagePath('./faulk.jpg'), 'Marshall Faulk Touchdown Run'),
+      new Video(75, [ new VideoSource('video/mp4', videoPath('./beckham.mp4')) ],
+        imagePath('./beckham.jpg'), 'Odell Beckham Jr. Catch'),
       new Video(49, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=iyRGH7HDs7s') ],
         imagePath('./tarasenko.png'), 'Vladimir Tarasenko goal'),
-      new Video(44, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=DojCj2xxwjA') ],
-        imagePath('./giba.png'), 'Giba Spike'),
-      new Video(28, [ new VideoSource('video/mp4', videoPath('./dante.mp4')) ],
-        imagePath('./dante.png'), 'Dante Spike'),
+      new Video(41, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=BGp8cdgVveY') ],
+        imagePath('./allen.jpg'), 'Jake Allen Save'),
+      new Video(91, [ new VideoSource('video/mp4', videoPath('./moss.mp4')) ],
+        imagePath('./moss.png'), 'Randy Moss Catch'),
       new Video(25, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=uosTUGDIdx0') ],
-        imagePath('./julio-jones.jpg'), 'Julio Jones Pregame Catch')
+        imagePath('./julio-jones.jpg'), 'Julio Jones Pregame Catch'),
+      // new Video(78, [ new VideoSource('video/youtube', 'https://www.youtube.com/watch?v=VkvTLOhm-TQ') ],
+      //   imagePath('./lebron.jpg'), 'Lebron James Buzzer Beater')
     ]);
   }
 
