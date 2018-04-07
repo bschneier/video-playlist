347380bf057cb776fe28c9db016cc97070f755c4
Brian Schneier <bschneier6@gmail.com>
2018-04-07 18:05:54 -0500

    Add tests for playlist service

--- app/playlist/playlist-service/playlist.service.spec.ts
+++ app/playlist/playlist-service/playlist.service.spec.ts
@@ -1,14 +1,39 @@
-import { TestBed, inject } from '@angular/core/testing';
 import { PlaylistService } from './playlist.service';
+import { Video } from '../../shared/types/video';
 
 describe('PlaylistService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PlaylistService]
-    });
-  });
+  let service: PlaylistService;
+  beforeEach(() => { service = new PlaylistService(); });
 
-  it('should be created', inject([PlaylistService], (service: PlaylistService) => {
-    expect(service).toBeTruthy();
-  }));
+  function compareVideoArrays(array1: Video[], array2: Video[]): boolean {
+    let arraysAreEqual = true;
+    array1.some((value, index) => {
+      if (value.title !== array2[index].title || value.length !== array2[index].length) {
+        arraysAreEqual = false;
+      }
+      return !arraysAreEqual;
+    });
+
+    return arraysAreEqual;
+  }
+
+  function areAllArraysDistinct(array1: Video[], array2: Video[], array3: Video[]): boolean {
+    return !(compareVideoArrays(array1, array2) || compareVideoArrays(array2, array3) ||
+      compareVideoArrays(array1, array3));
+  }
+
+  it('should shuffle videos', () => {
+    let areVideosShuffled = false;
+    areVideosShuffled = areAllArraysDistinct(service.getPlaylist(), service.getPlaylist(), service.getPlaylist());
+    if (!areVideosShuffled) {
+      areVideosShuffled = areAllArraysDistinct(service.getPlaylist(), service.getPlaylist(),
+        service.getPlaylist());
+      if (areVideosShuffled) {
+        areVideosShuffled = areAllArraysDistinct(service.getPlaylist(), service.getPlaylist(),
+          service.getPlaylist());
+      }
+    }
+
+    expect(areVideosShuffled).toBeTruthy();
+  });
 });
