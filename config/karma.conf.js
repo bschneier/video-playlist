b77626a5a043d9bbe57d94e258397358ec9b9d4d
Brian Schneier <bschneier6@gmail.com>
2018-05-04 14:10:03 -0500

    Unit test runner updates

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -10,12 +10,12 @@ module.exports = (config) => {
     preprocessors: {
       'karma.entry.js': ['webpack']
     },
-    reporters: ['progress', 'coverage-istanbul'],
+    reporters: ['progress', 'html', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     webpack: require('./webpack.test.js'),
     webpackServer: {
@@ -36,38 +36,32 @@ module.exports = (config) => {
     },
     coverageIstanbulReporter: {
       // reports can be any that are listed here: https://github.com/istanbuljs/istanbul-reports/tree/590e6b0089f67b723a1fdf57bc7ccc080ff189d7/lib
-      reports: ['html', 'text-summary'],
-      // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+      reports: ['html'],
       dir: path.join(__dirname, '../coverage'),
-      // if using webpack and pre-loaders, work around webpack breaking the source path
       fixWebpackSourcePaths: true,
-      // stop istanbul outputting messages like `File [${filename}] ignored, nothing could be mapped`
       skipFilesWithNoCoverage: true,
-      // Most reporters accept additional config options. You can pass these through the `report-config` option
-      'report-config': {
-        // all options available at: https://github.com/istanbuljs/istanbul-reports/blob/590e6b0089f67b723a1fdf57bc7ccc080ff189d7/lib/html/index.js#L135-L137
-        html: {
-          // outputs the report in ./coverage/html
-          subdir: 'html'
-        }
-      },
-      // enforce percentage thresholds
-      // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
       thresholds: {
-        emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
-        global: { // thresholds for all files
+        emitWarning: true,
+        global: {
           statements: 75,
           lines: 75,
           branches: 75,
           functions: 75
         },
-        each: { // thresholds per file
+        each: {
           statements: 75,
           lines: 75,
           branches: 75,
           functions: 75
         }
       }
+    },
+    htmlReporter: {
+      outputFile: path.join(__dirname, '../test-results/report.html'),
+      pageTitle: 'Video Playlist Tests',
+      groupSuites: true,
+      useCompactStyle: true,
+      useLegacyStyle: false
     }
   });
 };
